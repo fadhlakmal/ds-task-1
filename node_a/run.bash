0786fd68cba927b843955a7e7e1d2fa1ffe196d3
fadhlakmal <akmal.madany1@gmail.com>
2025-11-04 11:54:36 +0700

    first commit

---
+++ node_a/run.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+python3 peer_node.py \
+	  --name A --listen 0.0.0.0 5000 \
+	  --peers A@192.168.122.11:5000 B@192.168.122.12:5000 C@192.168.122.13:5000 D@192.168.122.14:5000 \
+	  --logger 192.168.122.10 9999 \
+	  --offset-ms 600 \
+	  --initiate-broadcast --msg "Hello from A"
+
